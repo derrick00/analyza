279fc7ecf7a014c12cd19bf8f28a2b31305a14ec
derick00 <muturiderrick10@gmail.com>
2023-02-02 18:00:06 +0300

    updated for mobile view

--- analyser/settings.py
+++ analyser/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'home/luffy/analyza/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
